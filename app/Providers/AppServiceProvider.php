9adfab050052aec4c190aeabd0afd5fdc65430e7
kamiya414 <kamiya414y@gmail.com>
2023-12-03 19:42:03 +0900

    カリキュラム１

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    // public function boot(): void
+    // {
+    //     //
+    // }
+    
+    public function boot()
     {
-        //
+        \URL::forceScheme("https"); // 追加
     }
 }
